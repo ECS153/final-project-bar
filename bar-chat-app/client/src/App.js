addbbb861ef4afa2fbab1e6e1c72d9a9b027cff9
Brandon Jones Gunaman <tkaktkik@gmail.com>
2020-05-12 14:22:49 -0700

    updated jsx/react for login
    put in the main design for the login page with username, password and log in button

--- bar-chat-app/client/src/App.js
+++ bar-chat-app/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 import Chat from './components/Chat';
 
 const App = () => {
