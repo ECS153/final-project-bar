4473185ac1ffa053a6c541328cb934458e70a80f
vincentssheng <32716945+vincentssheng@users.noreply.github.com>
2020-05-21 14:37:09 -0700

    Data model created

--- eth-bar/contracts/Bar.sol
+++ eth-bar/contracts/Bar.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract Bar {
     uint public msgCount = 0; // state variable
+
+    // Data Model
+    struct Message {
+        uint id;
+        string content;
+        // string sender;
+    }
+
+    mapping(uint => Message) public messages;
+
+    constructor() public {
+        createMessage("Hello World");
+    }
+
+    function createMessage(string memory _content) public {
+        msgCount ++;
+        messages[msgCount] = Message(msgCount, _content);
+    }
 }
+
