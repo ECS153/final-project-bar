f4328a12203d89a678e24afaf0ba48e8f7ee3e18
Brandon Jones Gunaman <tkaktkik@gmail.com>
2020-05-24 15:36:49 -0700

    Revert "testing web3 with changes to original blockchain app.js"
    This reverts commit 9e1270442967a8c68827aed1151220d9f7ee2505.

--- bar-chat-app/client/src/eth-bar/src/blockchain.js
+++ bar-chat-app/client/src/eth-bar/src/blockchain.js
@@ -30,9 +30,9 @@ class Blockchain {
     // connect blockchain to browser
     async loadWeb3() {
 
-        if (typeof this.web3 !== 'undefined') {
-            this.web3Provider = this.web3.currentProvider
-            this.web3 = new Web3(window.web3.currentProvider)
+        if (typeof window.web3 !== 'undefined') {
+            this.web3Provider = window.web3.currentProvider
+            window.web3 = new Web3(window.web3.currentProvider)
         } else {
             window.alert("Please connect to Metamask.")
         }
@@ -42,9 +42,8 @@ class Blockchain {
             try { 
                 window.ethereum.enable().then(function() {
                 // User has allowed account access to DApp...
-                    console.log("account okay")
-                    this.web3.eth.sendTransaction({/*....*/});
-                });
+                console.log("account okay")
+            });
             } catch(e) {
                 // User has denied account access to DApp...
                 console.log("account denied")
@@ -52,8 +51,7 @@ class Blockchain {
         }
         // Legacy DApp Browsers
         else if (window.web3) {
-            this.web3Provider = this.web3.currentProvider;
-            window.web3 = new Web3(this.web3.currentProvider);
+            window.web3 = new Web3(window.web3.currentProvider);
         }
         // Non-DApp Browsers
         else {
@@ -62,7 +60,7 @@ class Blockchain {
     }
 
     async loadAccount() {
-        this.account = this.web3.eth.accounts[0]
+        this.account = window.web3.eth.accounts[0]
         console.log(this.account)
     }
 
